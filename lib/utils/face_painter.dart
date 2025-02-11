81f941a2b8b8006b11e10c3dabfaa84a41de0e20
a7medjamal <ahmedjmal20003@gmail.com>
2025-02-11 06:46:07 +0200

    modify: face painter

--- lib/utils/face_painter.dart
+++ lib/utils/face_painter.dart
@@ -16,34 +16,25 @@ class FacePainter extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeWidth = 2.0
       ..color = Colors.red;
-
-    // Adjust the image size to fit inside the custom paint bounds
     final imageRect = Rect.fromLTWH(0, 0, size.width, size.height);
     canvas.drawImageRect(
         image,
         Rect.fromLTWH(0, 0, image.width.toDouble(), image.height.toDouble()),
         imageRect,
         Paint());
-
-    // Calculate scaling factor for bounding box
     double scaleX = size.width / image.width.toDouble();
     double scaleY = size.height / image.height.toDouble();
-
-    // Draw the face bounding boxes
     for (var face in faces) {
       final boundingBox = face.boundingBox;
-
       final scaledBoundingBox = Rect.fromLTRB(
         boundingBox.left * scaleX,
         boundingBox.top * scaleY,
         boundingBox.right * scaleX,
         boundingBox.bottom * scaleY,
       );
-
       canvas.drawRect(scaledBoundingBox, paint);
     }
   }
-
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
