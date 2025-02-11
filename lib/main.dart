bb066129fa37689342bdfad59a582dd2c9251fee
a7medjamal <ahmedjmal20003@gmail.com>
2025-02-11 06:37:37 +0200

    removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:face_detection_app/choose_or_capture.dart';
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(FaceDetection());
+}
+
 class FaceDetection extends StatelessWidget {
   const FaceDetection({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: ChooseOrCapture(),
     );
   }
